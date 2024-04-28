e7349b8ab0f3e40a00660608ca52f8d516b9523c
Al Gabriel Orig <jaworski.orig@gmail.com>
2024-04-28 23:14:12 +0800

    add cardGame

---
+++ bitwise-iOS/cardGame.swift
@@ -0,0 +1,18 @@
+//
+//  cardGame.swift
+//  bitwise-iOS
+//
+//  Created by Al Gabriel on 4/28/24.
+//
+
+import SwiftUI
+
+struct cardGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cardGame()
+}
