6c2e5e55b1504c436ba8a4956b464b126e4b7b0b
Al Gabriel Orig <jaworski.orig@gmail.com>
2024-04-28 23:18:14 +0800

    add cardGame logic and View

--- bitwise-iOS/cardGame.swift
+++ bitwise-iOS/cardGame.swift
@@ -8,8 +8,84 @@
 import SwiftUI
 
 struct cardGame: View {
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button{
+                    deal()
+                }label: {
+                Image("button")
+                }
+                
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                Spacer()
+            }
+        }
+    }
+    
+    func deal(){
+        // random players card
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // random cpu card
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // update scores
+        if playerCardValue > cpuCardValue {
+            
+            // add 1 to player score
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            // add 1 to cpu score
+            cpuScore += 1
+        }
     }
 }
 
