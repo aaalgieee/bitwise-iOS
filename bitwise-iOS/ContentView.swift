f90016a3e9f9b986695266396ac01490e473d360
Al Gabriel Orig <jaworski.orig@gmail.com>
2024-04-28 21:58:56 +0800

    Initial Commit

---
+++ bitwise-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bitwise-iOS
+//
+//  Created by Al Gabriel on 4/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bitwise-iOS/bitwise_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitwise_iOSApp.swift
+//  bitwise-iOS
+//
+//  Created by Al Gabriel on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct bitwise_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
